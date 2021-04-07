c553afbc987fea8e65d578ebc80034d828463bef
Richard <rdesilvey@gmail.com>
2021-04-07 10:47:43 -0600

    Refactor tests

--- spec/league_spec.rb
+++ spec/league_spec.rb
@@ -21,91 +21,43 @@ describe League do
 
   context '#initialize' do
     it 'exists' do
-      league = League.new("Premier League")
-      expect(league).to be_instance_of League
-      expect(league.name).to eq "Premier League"
-      expect(league.teams).to eq []
+      @league = League.new("Premier League")
+      expect(@league).to be_instance_of League
+      expect(@league.name).to eq "Premier League"
+      expect(@league.teams).to eq []
     end
   end
 
   context '#add_team' do
     it 'adds a team to the league' do
-      roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
-      sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
-      richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
-
-      jamie = Player.new({name: "Jamie Tartt", position: "Striker", salary: 1_500_000})
-      fernandinho = Player.new({name: "Fernandinho", position: "Midfielder", salary: 5_200_000})
-      manchester = Team.new("Manchester City FC", "Pep Guardiola", [jamie, fernandinho])
-
-      league = League.new("Premier League")
-      league.add_team(richmond)
-      league.add_team(manchester)
-      expected_teams = [richmond, manchester]
-
-      expect(league.teams).to eq expected_teams
+      expected_teams = [@richmond, @manchester]
+      expect(@league.teams).to eq expected_teams
     end
   end
 
   context '#captains' do
     it 'returns the captain of each team' do
-      roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
-      sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
-      richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
-
-      jamie = Player.new({name: "Jamie Tartt", position: "Striker", salary: 1_500_000})
-      fernandinho = Player.new({name: "Fernandinho", position: "Midfielder", salary: 5_200_000})
-      manchester = Team.new("Manchester City FC", "Pep Guardiola", [jamie, fernandinho])
-
-      league = League.new("Premier League")
-      league.add_team(richmond)
-      league.add_team(manchester)
-
-      expected_captains = [roy, fernandinho]
-
-      expect(league.captains).to eq expected_captains
+      expected_captains = [@roy, @fernandinho]
+      expect(@league.captains).to eq expected_captains
     end
   end
 
   context '#players_by_team' do
     it 'returns hash with team as key and player array as value' do
-      roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
-      sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
-      richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
-
-      jamie = Player.new({name: "Jamie Tartt", position: "Striker", salary: 1_500_000})
-      fernandinho = Player.new({name: "Fernandinho", position: "Midfielder", salary: 5_200_000})
-      manchester = Team.new("Manchester City FC", "Pep Guardiola", [jamie, fernandinho])
-
-      league = League.new("Premier League")
-      league.add_team(richmond)
-      league.add_team(manchester)
-
       expected_hash = {
-        richmond => ["Roy Kent", "Sam Obisanya"],
-        manchester => ["Jamie Tartt", "Fernandinho"]
+        @richmond => ["Roy Kent", "Sam Obisanya"],
+        @manchester => ["Jamie Tartt", "Fernandinho"]
       }
 
-      expect(league.players_by_team).to eq expected_hash
+      expect(@league.players_by_team).to eq expected_hash
     end
   end
 
   context '#most_expensive_player' do
     it 'returns the most expensive player in the league' do
-      roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
-      sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
-      richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
 
-      jamie = Player.new({name: "Jamie Tartt", position: "Striker", salary: 1_500_000})
-      fernandinho = Player.new({name: "Fernandinho", position: "Midfielder", salary: 5_200_000})
-      manchester = Team.new("Manchester City FC", "Pep Guardiola", [jamie, fernandinho])
-
-      league = League.new("Premier League")
-      league.add_team(richmond)
-      league.add_team(manchester)
-
-      most_expensive_player = league.most_expensive_player
-      expected_player = [fernandinho]
+      most_expensive_player = @league.most_expensive_player
+      expected_player = [@fernandinho]
 
       expect(most_expensive_player).to eq expected_player
     end
@@ -113,17 +65,6 @@ describe League do
 
   context '#players_by_salary_range' do
     it 'returns each players sorted by range in a hash' do
-      roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
-      sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
-      richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
-
-      jamie = Player.new({name: "Jamie Tartt", position: "Striker", salary: 1_500_000})
-      fernandinho = Player.new({name: "Fernandinho", position: "Midfielder", salary: 5_200_000})
-      manchester = Team.new("Manchester City FC", "Pep Guardiola", [jamie, fernandinho])
-
-      league = League.new("Premier League")
-      league.add_team(richmond)
-      league.add_team(manchester)
 
       expected_hash = {
        "Over 0M" => ["Sam Obisanya"],
@@ -134,7 +75,7 @@ describe League do
        "Over 5M" => ["Fernandinho"],
      }
 
-     players_by_salary_range_hash = league.players_by_salary_range
+     players_by_salary_range_hash = @league.players_by_salary_range
 
      expect(players_by_salary_range_hash).to eq expected_hash
     end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -3,22 +3,22 @@ require './lib/player'
 require 'rspec'
 
 describe Team do
+
+  before :each do
+    @roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
+    @sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
+    @team = Team.new("AFC Richmond", "Ted Lasso", [@roy, @sam])
+  end
+
   context '#initialize' do
     it 'exists' do
-      roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
-      sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
-      team = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
-      expect(team).to be_instance_of Team
+      expect(@team).to be_instance_of Team
     end
   end
 
   context '#total_salary' do
     it 'returns the total salary of the team' do
-      roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
-      sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
-      team = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
-
-      total_salary = team.total_salary
+      total_salary = @team.total_salary
       expected_total = 1_600_000
       expect(total_salary).to eq expected_total
     end
@@ -34,21 +34,13 @@ describe Team do
 
   context '#captain' do
     it 'returns the highest paid player as captain' do
-      roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
-      sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
-      team = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
-
-      expect(team.captain).to eq roy
+      expect(@team.captain).to eq @roy
     end
   end
 
   context '#positions_filled' do
     it 'returns the positions of all the players' do
-      roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
-      sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
-      team = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
-
-      positions_filled = team.positions_filled
+      positions_filled = @team.positions_filled
       expected_positions = ["Center Midfielder", "Right-back Defender"]
       expect(positions_filled).to eq expected_positions
     end
