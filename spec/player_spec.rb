e700b94f7bc8c16ab8ab65277f31d91a5e4d9824
Richard <rdesilvey@gmail.com>
2021-04-07 09:09:44 -0600

    Test attributes

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -6,6 +6,9 @@ describe Player do
     it 'exists' do
       player = Player.new({name: "Roy Kent", position: "Center Midfielder", salary: 1_000_000})
       expect(player).to be_instance_of Player
+      expect(player.name).to eq "Roy Kent"
+      expect(player.position).to eq "Center Midfielder"
+      expect(player.salary).to eq 1_000_000
     end
   end
 end
