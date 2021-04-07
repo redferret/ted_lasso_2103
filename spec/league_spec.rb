1c1406eecea523f0df2d4f97fd6536c53c02fbac
Richard <rdesilvey@gmail.com>
2021-04-07 09:29:22 -0600

    More tests

--- spec/league_spec.rb
+++ spec/league_spec.rb
@@ -6,8 +6,10 @@ require 'rspec'
 describe League do
   context '#initialize' do
     it 'exists' do
-      league = League.new
+      league = League.new("Premier League")
       expect(league).to be_instance_of League
+      expect(league.name).to eq "Premier League"
+      expect(league.teams).to eq []
     end
   end
 end
