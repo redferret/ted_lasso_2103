15680077d5f1d171937131f7eb0c831b4e67103b
Richard <rdesilvey@gmail.com>
2021-04-07 09:36:48 -0600

    Test captains method

--- spec/league_spec.rb
+++ spec/league_spec.rb
@@ -32,4 +32,24 @@ describe League do
     end
   end
 
+  context '#captains' do
+    it 'returns the captain of each team' do
+      roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
+      sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
+      richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
+
+      jamie = Player.new({name: "Jamie Tartt", position: "Striker", salary: 1_500_000})
+      fernandinho = Player.new({name: "Fernandinho", position: "Midfielder", salary: 5_200_000})
+      manchester = Team.new("Manchester City FC", "Pep Guardiola", [jamie, fernandinho])
+
+      league = League.new("Premier League")
+      league.add_team(richmond)
+      league.add_team(manchester)
+
+      expected_captains = [roy, fernandinho]
+
+      expect(league.captains).to eq expected_captains
+    end
+  end
+
 end
